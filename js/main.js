214c517229028c6b7c279feadc6acac6f7ba1146
Javlon <akmalovjavlonbek1028hp@gmail.com>
2022-03-25 16:00:56 +0500

    yangilandi

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// let name = 21
+// var dilshod_name
+// let 
+
+// let b = 15
+
+// let      a = 12
+
+// console.log(b+a)
+
+// console.log(b);
+
+// b = 13 + a
+
+// a = 13
+
+// console.log(a);
+
+
+//  matematik  amallar
+
+// + , - , * , / , % , ** , () , 
+
+// console.log(16**0.5);
+
+// console.log(16-7);
+// console.log(19/2);
+
+console.log(19%4);  // 3
+console.log(16%12); // 4
+console.log(22%5);  // 2
+console.log(12*11); // 132
+
